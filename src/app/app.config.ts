6e7c28bf7c3f4355731fa877401875a363eb483e
Matthias Gutsch <MGutsch@datacolor.com>
2026-03-26 12:46:56 +0100

    ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,20 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { LucideAngularModule, Settings } from 'lucide-angular';
+import { LUCIDE_ICONS, LucideIconProvider, icons } from 'lucide-angular';
 
 import { routes } from './app.routes';
-import { MyDiamond, MyBolt, Speedo } from './icons/custom-icons';
+import * as customIcons from './icons/custom-icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    importProvidersFrom(
-      LucideAngularModule.pick({ Settings, MyDiamond, MyBolt, Speedo })
-    ),
+    {
+      provide: LUCIDE_ICONS,
+      multi: true,
+      useValue: new LucideIconProvider({ ...icons, ...customIcons }),
+    },
   ]
 };
