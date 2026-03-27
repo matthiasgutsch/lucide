bae91f5bbe690beea6620a4136a043fce2d1235b
Matthias Gutsch <MGutsch@datacolor.com>
2026-03-27 09:23:13 +0100

    ok

--- src/app/icons/custom-icons.ts
+++ src/app/icons/custom-icons.ts
@@ -1,17 +1,41 @@
 import { LucideIconData } from 'lucide-angular';
 
-export const DcDiamond: LucideIconData = [['polygon', { points: '12 2 22 12 12 22 2 12' }]];
+export const DcDiamond: LucideIconData = [
+  ['polygon', { points: '12 2 22 12 12 22 2 12', 'shape-rendering': 'geometricPrecision' }],
+];
 
-export const DcBolt: LucideIconData = [['path', { d: 'M13 2L3 14h9l-1 8 10-12h-9l1-8z' }]];
+export const DcBolt: LucideIconData = [
+  [
+    'path',
+    {
+      d: 'M13 2L3 14h9l-1 8 10-12h-9l1-8z',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+      'shape-rendering': 'geometricPrecision',
+    },
+  ],
+];
 
 export const DcSpeedo: LucideIconData = [
   [
     'path',
     {
-      d: 'M12,24a12,12,0,1,1,12,-12A12,12,0,0,1,12,24Zm0,-22a10,10,0,1,0,10,10A10,10,0,0,0,12,2Zm8,10.007V12h-2a6,6,0,0,0-12,0h-2a8,8,0,0,1,16,0v0.007Zm-8.951,1.162a0.962,0.962,0,0,1-.035-0.176v-3.985a0.986,0.986,0,1,1,1.972,0v3.985a0.935,0.935,0,0,1-.035.175A3,3,0,1,1,11.049,13.169ZM12,17a1,1,0,1,0-1-1A1,1,0,0,0,12,17Z',
-      fill: 'currentColor',
-      'fill-rule': 'evenodd',
-      stroke: 'none',
+      d: 'M5 19a9 9 0 1 1 14 0',
+      'stroke-linecap': 'round',
+      'shape-rendering': 'geometricPrecision',
     },
   ],
+  [
+    'path',
+    {
+      d: 'M8 19a6 6 0 0 1 8 0',
+      'stroke-linecap': 'round',
+      'shape-rendering': 'geometricPrecision',
+    },
+  ],
+  [
+    'path',
+    { d: 'M12 13L16 8', 'stroke-linecap': 'round', 'shape-rendering': 'geometricPrecision' },
+  ],
+  ['circle', { cx: '12', cy: '13', r: '1', 'shape-rendering': 'geometricPrecision' }],
 ];
