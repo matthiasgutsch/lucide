85847bee9be80f295d66caef11e3253631087574
Matthias Gutsch <MGutsch@datacolor.com>
2026-04-28 10:18:18 +0200

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { IconMap } from './icon-map/icon-map';
 
 export const routes: Routes = [
+  { path: '', component: IconMap },
   { path: 'icon-map', component: IconMap },
 ];
